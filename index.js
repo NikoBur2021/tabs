f823fd49b5e7d443475c8f08ead6bc05c0384bc6
Niko <88543699+NikoBur2021@users.noreply.github.com>
2021-09-23 10:28:25 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const tabs = document.querySelector('.tabs')
+const boxContent = document.querySelector('.box-content')
+boxContent.children[0].classList.add('show')
+let index = 0
+tabs.onclick = (event) => {
+    const parentsItem = event.target.closest('.tab')
+    if (parentsItem !== null) {
+        tabs.children[index].classList.remove('active')
+        boxContent.children[index].classList.remove('show')
+        index = parentsItem.dataset.index
+        boxContent.children[index].classList.add('show')
+        tabs.children[index].classList.add('active')
+    }
+}
